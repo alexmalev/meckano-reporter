9e6781e1f89cbfda4ebf3d8723f4e2a0828edcf8
alexmalev <alex.m@guardz.com>
2024-12-25 10:28:42 +0200

    fix

--- tests/attendance.spec.ts
+++ tests/attendance.spec.ts
@@ -110,7 +110,7 @@ test("fill hours", async ({ page }) => {
     await rowToFill.locator("input.checkOut").fill(endTime.replace(":", ""));
   }
 
-  // await page.locator("button.update-freeReporting").click();
+  await page.locator("button.update-freeReporting").click();
 
   logger.log(
     "Waiting for the report to be submitted. this will take a couple of seconds",
